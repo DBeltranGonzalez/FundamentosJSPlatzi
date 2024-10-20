581abaf709bcab818961ab3521b00c0049e443ae
Diego Beltran <diego.beltrang@icloud.com>
2024-10-19 19:58:46 -0500

    Archivo de variables, con comentarios de cosas permitidas y no permitidas o no recomendadas

---
+++ 1_variables.js
@@ -0,0 +1,24 @@
+let cajaDeAndy = 'Woody';
+console.log(cajaDeAndy);
+
+/*
+NO PERMITIDO para variables
+
+let c = 'Woody' -> usar solo un caracter
+let cda -> no es descriptivo
+let pcAndy -> tiene ambigüedad
+let c, cda 
+    pcAndy -> desordenadas porque no van en la misma línea
+*/
+
+/* 
+PERMITIDO para variables
+
+let primerTrasteoDeAndy -> Es descriptiva
+let urlDelUsuario -> URL es una abreviación permitido porque se usan a nivel general
+*/
+
+/*
+let -> variables que van a cambiar en el tiempo --> let contador
+const -> variables constantes o fijas que siempre tendrán el mismo valor --> const pi = 3.1415
+*/
