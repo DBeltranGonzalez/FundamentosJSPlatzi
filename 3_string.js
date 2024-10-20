b7b63ea5a350a56f8734228ca344871195d44c98
Diego Beltran <diego.beltrang@icloud.com>
2024-10-20 16:12:27 -0500

    Añadiendo archivo de strings

---
+++ 3_string.js
@@ -0,0 +1,16 @@
+let string1 = 'Hola, mundo';
+let string2 = "JS es genial";
+let string3 = `${string1} feliz :)`; //Comillas dobles para concatenar
+
+let concatenacion = string1 + ' ' + string2;
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(concatenacion);
+
+let frase = 'JavaScript es extremadamente genial';
+console.log(frase.length); //Conteo de caracteres
+console.log(frase.toLowerCase()); //Pasar todo a minúsculas
+console.log(frase.toUpperCase()); //Pasar todo a mayúsculas
+console.log(frase.substring(0,10)); 
