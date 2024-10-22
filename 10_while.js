9e759a1f426341ae5cfb3a3a3964430a46b4bec0
Diego Beltran <diego.beltrang@icloud.com>
2024-10-21 20:13:12 -0500

    Actualización de do while

--- 10_while.js
+++ 10_while.js
@@ -1,6 +1,20 @@
+//while:
+
 let contador = 0;
+console.log('Ciclo while:');
 
 while (contador < 10) {
     console.log(contador);
     contador++;
 }
+
+// do while:
+
+console.log('Ciclo do while:');
+
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while(i < 0);
