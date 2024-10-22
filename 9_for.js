01e1a423d037b1d1c1c037e5682fa3e29c89cfc3
Diego Beltran <diego.beltrang@icloud.com>
2024-10-21 19:55:44 -0500

    Actualización del archivo for con la estructura for of

--- 9_for.js
+++ 9_for.js
@@ -1,5 +1,24 @@
 let list = ['eat', 'sleep', 'code', 'repeat'];
+console.log('for tradicional:');
 
+// For tradicional
 for (let i = 0; i < list.length; i++) {
-    console.log(list[i])
+    console.log(list[i]);
+}
+
+/* 
+    for of: por cada elemento de cierta cosa, vamos a hacer...
+
+    se utiliza con objetos iterables(arrays y strings)
+
+    for (variable of objeto) {
+        código a ejecutar;
+    }
+*/
+
+let canasta = ['manzana', 'pera', 'naranja', 'uva'];
+console.log('for of:');
+
+for (fruta of canasta) {
+    console.log(fruta)
 }
