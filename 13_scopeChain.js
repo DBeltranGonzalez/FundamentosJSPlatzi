f151da5c809525217685455aaf4152716bd76e3b
Diego Beltran <diego.beltrang@icloud.com>
2024-10-22 20:51:13 -0500

    Añadiendo comentario a archivo scope

--- 13_scopeChain.js
+++ 13_scopeChain.js
@@ -9,6 +9,20 @@ function getProductDetails() {
     return `The ${productName} costs $${price} and is from the brand ${brand}`
 }
 
-console.log(getProductDetails()) // The Laptop costs $899 and is from the brand TechCo
+console.log(getProductDetails()) 
+// The Laptop costs $899 and is from the brand TechCo;
 
 console.log(`The ${productName} costs $${price} and is from the brand ${brand}`) 
+// The Smartphone costs $499 and is from the brand TechCo;
+
+/* 
+Contexto global: variables globales
+Contexto local: variables locales, almacenado en bloques {}
+
+    1. function
+    2. for
+    3. while
+    4. if
+
+Del contexto global no se puede acceder a contextos locales, pero los locales si pueden acceder al global
+*/
