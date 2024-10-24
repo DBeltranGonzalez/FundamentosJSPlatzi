87affcae9039753f217c806d6ac986b053e9f5bc
Diego Beltran <diego.beltrang@icloud.com>
2024-10-24 17:55:24 -0500

    Añadiendo los conceptos de filter y reduce

--- 16_metodosArrays.js
+++ 16_metodosArrays.js
@@ -40,3 +40,37 @@ numbersFibonacci.forEach(element => {
 });
 console.log(numbersFibonacci);
 console.log(sumaFibonacci);
+
+// 4. filter(): crear un array con los elementos que cumplan la condición en el filter
+
+const tablaDelCinco = [5,10,15,20,25,30,35,40];
+const decenas = tablaDelCinco.filter(numero => numero % 10 === 0);
+
+console.log(tablaDelCinco);
+console.log(decenas);
+
+// 5. reduce(): Devuelve un valor único
+
+//reduce caso 1
+
+let acumulador = 0
+const factoriales = [1,2,6,24,120]
+const sumaFactoriales = factoriales.reduce((acumulador, valorActual) => acumulador + valorActual, 0); // 0 es el valor inicial
+
+console.log(factoriales)
+console.log(sumaFactoriales)
+
+// reduce caso 2
+
+const paises = ['colombia', 'brasil', 'argentina', 'brasil', 'colombia', 'brasil', 'argentina', 'brasil', 'paraguay', 'brasil', 'colombia', 'argentina', 'colombia']
+
+const paisesFrecuencia = paises.reduce((accumulator, currentValue) => {
+    if(accumulator[currentValue]) {
+        accumulator[currentValue] ++;
+    } else {
+        accumulator[currentValue] = 1;
+    }
+    return accumulator
+}, {})
+
+console.log(paisesFrecuencia);
