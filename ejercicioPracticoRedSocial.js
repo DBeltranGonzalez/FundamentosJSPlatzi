8b64af30f7d18b7140a7a3d1854ff4a77ecb68f6
Diego Beltran <diego.beltrang@icloud.com>
2024-10-29 18:53:37 -0500

    Inclusión del archivo para el proyecto de crear una red social

---
+++ ejercicioPracticoRedSocial.js
@@ -0,0 +1,42 @@
+/*
+Requerimientos del reto:
+
+1. El usuario debe poder ingresar su usuario y contraseña
+2. El sistema debe ser capaz de validar si el usuario y contraseña ingresados por el usuario exiten en la base de datos
+3. Si el usuario y contraseña son correctos, el sistema debe mostrar un mensaje de bienvenia y mostrar el timeline del usuario.
+4. Si el usuario y contraseña son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningún timeline.
+*/
+
+const usersDatabase = [
+    {
+        username: 'andres',
+        password: '123',
+    },
+    {
+        username: 'caro',
+        password: '456',
+    },
+    {
+        username: 'mariana',
+        password: '789',
+    },
+];
+
+const usersTimeLine = [
+    {
+        username: 'Estefany',
+        timeline: 'Me encanta JS',
+    },
+    {
+        username: 'Oscar',
+        timeline: 'Bebeloper es lo mejor!!!',
+    },
+    {
+        username: 'Mariana',
+        timeline: 'A mí me gusta más el café que el te',
+    },
+    {
+        username: 'Andres',
+        timeline: 'Hoy no se trabaja :D',
+    },
+];
