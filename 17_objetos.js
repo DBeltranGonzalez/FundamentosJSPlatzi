c50d35f892c17d165e6e4258527bee2fa562d0b9
Diego Beltran <diego.beltrang@icloud.com>
2024-10-28 19:37:41 -0500

    Agregando y eliminando propiedades del objeto

--- 17_objetos.js
+++ 17_objetos.js
@@ -6,7 +6,9 @@
     objeto {
         propiedad: valor,
         propiedad: valor,
-        propiedad: valor
+        propiedad: valor,
+
+        metodos()
     }
 */
 
@@ -21,3 +23,18 @@ const persona = {
         console.log(`Hola, mi nombre es ${persona.nombre}`)
     }
 }
+
+console.log(persona);
+persona.saludar();
+
+persona.telefono = '555-555-5555'; //Agregar propiedades al objeto
+console.log(persona.telefono);
+
+persona.despedir = () => { // Agregar un método al objeto
+    console.log('Adiós'); 
+}
+
+persona.despedir();
+
+delete persona.telefono; // Eliminar una propiedad del objeto
+delete persona.despedir; // Eliminar un método del objeto
